1538f3cb5714b67c6c0684d10cbde08f8b138009
marcoschndlr <58122429+marcoschndlr@users.noreply.github.com>
2026-05-31 15:23:47 +0200

    chore: init frontend and backend

---
+++ backend/src/main/java/social/marco/dotodo/DotodoApplication.java
@@ -0,0 +1,13 @@
+package social.marco.dotodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotodoApplication.class, args);
+	}
+
+}
